7fdc31e131e54ed155342638909d788cb044a978
cormullion <cormullion@mac.com>
2016-11-18 13:10:49 +0000

    minor edits

--- src/Mellan.jl
+++ src/Mellan.jl
@@ -1,43 +1,37 @@
-VERSION >= v"0.4.0-dev+6641" && __precompile__()
+VERSION >= v"0.4.0" && __precompile__()
 
 module Mellan
 
 export mellanize
 
-println("loading Luxor")
-using Luxor
-println("loading Colors")
-using Colors
-println("importing Images")
+using Luxor, Colors
 import Images
-println("finished loading")
 
-function getpixel(grayimage, x, y)
+# TODO: too much flipping between Int and Float here!
+function getpixel(grayimage, x, y, imagewidth)
     # get grey value of pixel at x/y from image
     # since origin is in the middle of the page,
+    x1 = 1 + x + (imagewidth ÷ 2)
+    y1 = 1 + y + (imagewidth ÷ 2)
     # x and y are between -imagewidth/2 and +imagewidth/2
     # convert to values between 1 (not 0!) and imagewidth
-    global imagewidth
-    x1 = 1 + x + (imagewidth/2)
-    y1 = 1 + y + (imagewidth/2)
     return float(1 - grayimage[convert(Int, floor(x1)), convert(Int, floor(y1))])
 end
 
-# imagefile is path of 8bit PNG or JPG
-# side is the required image size of sides
 function mellanize(imagefile, side;
-        linescaler      = 5, # multiply the grey value of a pixel [0,1] to get this width of line
+        # imagefile is path of 8bit PNG or JPG
+        # side is the required image size of sides
+        linescaler      = 5.0, # multiply the grey value of a pixel [0,1] to get this width of line
         foregroundcolor = "black",
         backgroundcolor = "antiquewhite2",
-        startradius     = 5,   # starting radius
+        startradius     = 5.0,   # starting radius
         margin          = 15,
-        awaystep        = 2,  # controls how much the radius lengthens for each step,
-        chord           = 10, # length of each stroke
+        awaystep        = 2.0,  # controls how much the radius lengthens for each step,
+        chord           = 10.0, # length of each stroke
         annotation      = false
         )
-    tic()
     pageside = side
-    global imagewidth = pageside - margin
+    imagewidth = pageside - margin
     outputfilename = splitext(imagefile)[1] * "-mellan-$(side).pdf"
     Drawing(pageside, pageside, outputfilename)
     img1 = Images.load(imagefile)
@@ -53,7 +47,8 @@ function mellanize(imagefile, side;
     radius1 = radius2 = 0
     imw2 = imagewidth/2
     sqrt2 = sqrt(2)
-    while radius2 < (imw2 * sqrt2) # while larger radius is less than diagonal distance from center to corner
+    imsq = imw2 * sqrt2
+    while radius2 < imsq # while larger radius is less than diagonal distance from center to corner
         radius1 = startradius + (awaystep * theta)
         around1 = mod2pi(-theta)
         theta += chord/radius1
@@ -69,7 +64,7 @@ function mellanize(imagefile, side;
                abs(startpoint.y) <  imw2 &&
                abs(endpoint.x)   <  imw2 &&
                abs(endpoint.y)   <  imw2
-                    setline(linescaler * getpixel(grayimage, startpoint.x, startpoint.y)) # actually should be average of start and end...
+                    setline(linescaler * getpixel(grayimage, startpoint.x, startpoint.y, imagewidth)) # actually should be average of start and end...
             else
                 setline(0)
             end
@@ -86,7 +81,6 @@ function mellanize(imagefile, side;
     end
     finish()
     preview()
-    return toc()
 end
 
 end
